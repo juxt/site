9a0611388016be9a08275cf374c2832320ba9cbb
Malcolm Sparks <mal@juxt.pro>
2021-02-24 10:27:13 +0000

    Add main runner

---
+++ src/juxt/site/alpha/main.clj
@@ -0,0 +1,19 @@
+;; Copyright © 2021, JUXT LTD.
+
+(ns juxt.site.alpha.main
+  (:require
+   edge.system
+   [integrant.core :as ig]))
+
+(def system nil)
+
+(defn -main [& args]
+  (let [system-config (edge.system/system-config {:profile :prod})
+        sys (ig/init system-config)]
+    (.addShutdownHook
+     (Runtime/getRuntime)
+     (Thread.
+      (fn []
+        (ig/halt! sys))))
+    (alter-var-root #'system (constantly sys)))
+  @(promise))
