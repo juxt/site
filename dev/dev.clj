870320219bccbd59057b19fb8fa10446f5f444d3
Malcolm Sparks <mal@juxt.pro>
2021-02-25 06:17:58 +0000

    Reorganize dev code structure

--- dev/dev.clj
+++ dev/dev.clj
@@ -3,7 +3,8 @@
 (ns dev
   (:require
    [dev-extras :refer :all]
-   [juxt.site.alpha.dev-extras :refer :all]))
+   [juxt.site.alpha.dev-extras :refer :all]
+   [juxt.site.alpha.init :refer [init-db]]))
 
 (set! *print-length* 100)
 (set! *print-level* 4)
