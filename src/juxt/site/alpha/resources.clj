4caf28c5356752c3d39d49ea6cb21ee20d1f5231
Malcolm Sparks <mal@juxt.pro>
2021-04-23 14:50:40 +0100

    Add better error report on 207

--- src/juxt/site/alpha/resources.clj
+++ src/juxt/site/alpha/resources.clj
@@ -4,7 +4,8 @@
   (:require
    [clojure.edn :as edn]
    [crux.api :as x]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.tools.logging :as log]))
 
 (alias 'http (create-ns 'juxt.http.alpha))
 (alias 'apex (create-ns 'juxt.apex.alpha))
@@ -28,10 +29,40 @@
                      (java.io.PushbackReader.
                       (java.io.InputStreamReader.
                        (java.io.ByteArrayInputStream.
-                        (::http/body request-instance)))))]
+                        (::http/body request-instance)))))
+          results
+          (doall
+           (for [resource resources]
+             (let [uri (:crux.db/id resource)
+                   existing? (when uri (x/entity db uri))]
+               (try
+                 (let [tx (x/submit-tx crux-node [[:crux.tx/put resource]])]
+                   (cond-> {:status (if existing? 204 201)
+                            :tx tx}
+                     uri (assoc :uri uri)))
+                 (catch Exception e
+                   (log/error e "Failed to submit resource")
+                   (cond-> {:status 400
+                            :error (.getMessage e)}
+                     uri (assoc :uri uri)))))))]
 
-      (->>
-       (x/submit-tx crux-node (mapv (fn [resource] [:crux.tx/put resource]) resources))
-       (x/await-tx crux-node))
+      (when-let [last-tx (reverse (filter :tx results))]
+        (x/await-tx crux-node last-tx))
 
-      (assoc req :ring.response/status 200))))
+      (let [status (case (count results)
+                     0 400
+                     1 (:status (first results))
+                     207)]
+        (cond->
+            (assoc req
+                   :ring.response/status status
+                   :ring.response/body
+                   ;; TODO: For default error bodies we shouldn't add the body here
+                   ;; and let an error handler put in the default message.
+                   (case status
+                     400 "Bad Request\r\n"
+                     201 "Created\r\n"
+                     204 "No Content\r\n"
+                     207 (pr-str (map (fn [r] (dissoc r :tx)) results))))
+            (and (#{201 204} status) (:uri (first results)))
+            (assoc-in [:ring.response/headers "location"] (:uri (first results))))))))
