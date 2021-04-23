376a151203ce9520675ac2f1b5d2494def151a48
Malcolm Sparks <mal@juxt.pro>
2021-04-23 10:57:52 +0100

    Allow multiple resources to be POSTed

--- src/juxt/site/alpha/resources.clj
+++ src/juxt/site/alpha/resources.clj
@@ -3,26 +3,35 @@
 (ns juxt.site.alpha.resources
   (:require
    [clojure.edn :as edn]
-   [crux.api :as x]))
+   [crux.api :as x]
+   [clojure.java.io :as io]))
 
 (alias 'http (create-ns 'juxt.http.alpha))
 (alias 'apex (create-ns 'juxt.apex.alpha))
 (alias 'site (create-ns 'juxt.site.alpha))
+
+(defn read-forms [r]
+  (lazy-seq
+   (let [res (edn/read {:eof :eof} r)]
+     (when-not (= res :eof)
+       (cons res (read-forms r))))))
 
 (defn post-resource
   "Post a new resource, or overwrite an existing one, in the database."
   [{::site/keys [db crux-node]
     ::apex/keys [request-instance] :as req}]
 
-  (let [resource (edn/read-string (slurp (::http/body request-instance)))
-        exists? (x/entity db (:crux.db/id resource))]
-    (case (::http/content-type request-instance)
-      "application/edn"
-      (do
-        (->>
-         (x/submit-tx crux-node [[:crux.tx/put resource]])
-         (x/await-tx crux-node))
+  (case (::http/content-type request-instance)
+    "application/edn"
 
-        (-> req
-            (assoc :ring.response/status (if-not exists? 201 204))
-            (update :ring.response/headers assoc "location" (:crux.db/id resource)))))))
+    (let [resources (read-forms
+                     (java.io.PushbackReader.
+                      (java.io.InputStreamReader.
+                       (java.io.ByteArrayInputStream.
+                        (::http/body request-instance)))))]
+
+      (->>
+       (x/submit-tx crux-node (mapv (fn [resource] [:crux.tx/put resource]) resources))
+       (x/await-tx crux-node))
+
+      (assoc req :ring.response/status 200))))
