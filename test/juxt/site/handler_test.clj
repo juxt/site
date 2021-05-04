8f6e658db5b6a45caa4b1b76f604292c39ae7d21
Malcolm Sparks <mal@juxt.pro>
2021-05-04 21:58:28 +0100

    Extract test utils

--- test/juxt/site/handler_test.clj
+++ test/juxt/site/handler_test.clj
@@ -7,12 +7,11 @@
    [crux.api :as x]
    [jsonista.core :as json]
    [juxt.reap.alpha.encoders :refer [format-http-date]]
-   [juxt.site.alpha.handler :as h]
    [juxt.mail.alpha.mail :as mailer]
-   [clojure.edn :as edn]
-   [clojure.java.io :as io])
+   [juxt.test.util :refer [with-crux with-handler submit-and-await!
+                           *crux-node* *handler*
+                           access-all-areas access-all-apis]])
   (:import
-   (crux.api ICruxAPI)
    (java.io ByteArrayInputStream)))
 
 (alias 'apex (create-ns 'juxt.apex.alpha))
@@ -21,56 +20,7 @@
 (alias 'pass (create-ns 'juxt.pass.alpha))
 (alias 'site (create-ns 'juxt.site.alpha))
 
-(def ^:dynamic *opts* {})
-(def ^:dynamic ^ICruxAPI *crux-node*)
-(def ^:dynamic *handler*)
-(def ^:dynamic *db*)
-
-(defn with-crux [f]
-  (with-open [node (x/start-node *opts*)]
-    (binding [*crux-node* node]
-      (f))))
-
-(defn submit-and-await! [transactions]
-  (->>
-   (x/submit-tx *crux-node* transactions)
-   (x/await-tx *crux-node*)))
-
-
-(defn make-handler [opts]
-  ((apply comp
-          (remove
-           #{h/wrap-healthcheck
-             h/wrap-ring-1-adapter}
-           (h/make-pipeline opts)))
-   identity))
-
-(defn with-handler [f]
-  (binding [*handler* (make-handler
-                       {::site/crux-node *crux-node*
-                        ::site/base-uri "https://example.org"
-                        ::site/uri-prefix "https://example.org"})]
-    (f)))
-
-(defn with-db [f]
-  (binding [*db* (x/db *crux-node*)]
-    (f)))
-
 (t/use-fixtures :each with-crux with-handler)
-
-(def access-all-areas
-  {:crux.db/id "https://example.org/access-rule"
-   ::site/description "A rule allowing access everything"
-   ::site/type "Rule"
-   ::pass/target '[]
-   ::pass/effect ::pass/allow})
-
-(def access-all-apis
-  {:crux.db/id "https://example.org/access-rule"
-   ::site/description "A rule allowing access to all APIs"
-   ::site/type "Rule"
-   ::pass/target '[[resource ::site/resource-provider ::apex/openapi-path]]
-   ::pass/effect ::pass/allow})
 
 (deftest put-test
   (submit-and-await!
---
+++ test/juxt/test/util.clj
@@ -0,0 +1,63 @@
+;; Copyright © 2021, JUXT LTD.
+
+(ns juxt.test.util
+  (:require
+   [juxt.site.alpha.handler :as h]
+   [crux.api :as x])
+  (:import
+   (crux.api ICruxAPI)))
+
+(def ^:dynamic *opts* {})
+(def ^:dynamic ^ICruxAPI *crux-node*)
+(def ^:dynamic *handler*)
+(def ^:dynamic *db*)
+
+(alias 'apex (create-ns 'juxt.apex.alpha))
+(alias 'http (create-ns 'juxt.http.alpha))
+(alias 'mail (create-ns 'juxt.mail.alpha))
+(alias 'pass (create-ns 'juxt.pass.alpha))
+(alias 'site (create-ns 'juxt.site.alpha))
+
+(defn with-crux [f]
+  (with-open [node (x/start-node *opts*)]
+    (binding [*crux-node* node]
+      (f))))
+
+(defn submit-and-await! [transactions]
+  (->>
+   (x/submit-tx *crux-node* transactions)
+   (x/await-tx *crux-node*)))
+
+
+(defn make-handler [opts]
+  ((apply comp
+          (remove
+           #{h/wrap-healthcheck
+             h/wrap-ring-1-adapter}
+           (h/make-pipeline opts)))
+   identity))
+
+(defn with-handler [f]
+  (binding [*handler* (make-handler
+                       {::site/crux-node *crux-node*
+                        ::site/base-uri "https://example.org"
+                        ::site/uri-prefix "https://example.org"})]
+    (f)))
+
+(defn with-db [f]
+  (binding [*db* (x/db *crux-node*)]
+    (f)))
+
+(def access-all-areas
+  {:crux.db/id "https://example.org/access-rule"
+   ::site/description "A rule allowing access everything"
+   ::site/type "Rule"
+   ::pass/target '[]
+   ::pass/effect ::pass/allow})
+
+(def access-all-apis
+  {:crux.db/id "https://example.org/access-rule"
+   ::site/description "A rule allowing access to all APIs"
+   ::site/type "Rule"
+   ::pass/target '[[resource ::site/resource-provider ::apex/openapi-path]]
+   ::pass/effect ::pass/allow})
