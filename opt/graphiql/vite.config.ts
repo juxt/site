8d716f83936dd7c4c93d98613d0dd8a6257c9074
Alex Davis <alx@juxt.pro>
2021-10-13 21:54:47 +0100

    Use vite instead of create react app
    apparently CRA is dead and you should use vite instead. All I know is the app
    now starts up in literally 0.5 seconds compared to 10 seconds before. Build
    times are 4x quicker

---
+++ opt/graphiql/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: "build",
+  },
+  plugins: [react()],
+});
