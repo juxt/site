984f7b48c4b8329367e6fdb1ab972750580ac74b
Alex Davis <alx@juxt.pro>
2021-11-19 17:07:02 +0000

    Pass more of the typesense response back

--- src/juxt/site/alpha/typesense.clj
+++ src/juxt/site/alpha/typesense.clj
@@ -6,14 +6,6 @@
 
 (def ts-settings {:uri "http://192.168.1.26:8108" :key "xyz"})
 
-(defn prepare-doc
-  [{:keys [document highlights]} found out-of]
-  (-> document
-      (assoc :found found
-             :outOf out-of
-             :matched (flatten (map :matched_tokens highlights)))
-      (rename-keys {:id :xt/id :type :juxt.site/type})))
-
 (defn operator->string
   [operator]
   (case operator
@@ -29,30 +21,58 @@
           (str (name filter-key) (operator->string operator) value))]
     (str/join " && " filter-array)))
 
+(defn search-terms->query
+  "Given a map of search terms, creates a typesense q and query_by value following
+  the following algorithm: If search-terms contains a single key named 'search',
+  assume :query_by will be set manually by the client. Otherwise, use the keys
+  as the input to 'query-by'"
+  [search-terms]
+  (let [query (or (some->> search-terms vals (str/join " ")) "*")
+        query-by (some->> search-terms
+                          keys
+                          (remove #{"search"})
+                          (map name)
+                          (str/join ", "))]
+    {:q (if (seq query) query "*")
+     :query_by query-by}))
+
+(defn update-doc-keys
+  "Renames the keys inside each hit document that comes from typesense so site can
+  handle it up the chain"
+  [results]
+  (def results results)
+  (update results :hits (fn [hits]
+                          (mapv (fn [hit]
+                                  (update hit :document
+                                          #(rename-keys
+                                            % {:id :xt/id
+                                               :type :juxt.site/type})))
+                                hits))))
+
 (defn ts-search
-  [collection site-args argument-values]
+  [site-args argument-values]
+  (def site-args site-args)
   (let [search-terms (get argument-values "searchTerms")
-        limit (get argument-values "limit")
-        offset (get argument-values "offset")
-        query (or (some->> search-terms vals (str/join " ")) "*")
-        query-by (some->> search-terms keys (map name) (str/join ", "))
+        limit (or (get argument-values "limit") 10)
+        offset (or (get argument-values "offset") 0)
         config (get site-args "typesenseConfig")
-        _ (def config config)
+        collection (:typesenseIndex config)
         filters (get site-args "filters")
         processed-filters (process-filters filters)
-        {:keys [hits found out_of] :as results}
+        results
         (first
          (client/search ts-settings collection
                         (merge
-                         {:q (if (seq query) query "*")
-                          :query_by query-by
-                          :filter_by processed-filters
+                         {:filter_by processed-filters
                           :per_page limit
-                          :page (+ 1 (/ (or offset 0) limit))}
+                          :page (+ 1 (long (/ offset limit)))}
+                         (search-terms->query search-terms)
                          config)))]
-    (def results results)
-    (map (fn [hit] (prepare-doc hit found out_of)) hits)))
+    (update-doc-keys results)))
 
 (defn count
   [collection search-terms]
-  (:found (first (client/search ts-settings collection {:q "*" :per_page 1 :page 1}))))
+  (:found (first
+           (client/search
+            ts-settings collection (merge (search-terms->query search-terms)
+                                          {:per_page 1 :page 1})))))
