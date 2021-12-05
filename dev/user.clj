3d300da7eea59d39378a2cea6bb731556bda64bf
Malcolm Sparks <mal@juxt.pro>
2021-12-05 09:20:58 +0000

    Delete local access token on startup
    This helps to avoid 401s in dev when the access token is stale (with respect to
    the restarted server).
    
    This is a temporary workaround to be replaced when access tokens are stored in
    the database.

--- dev/user.clj
+++ dev/user.clj
@@ -6,6 +6,7 @@
    [io.aviso.ansi :as ansi]
    [juxt.site.alpha.main :as main]
    [clojure.tools.logging :as log]
+   [clojure.java.io :as io]
    [juxt.site.alpha.repl :refer :all]
    [integrant.core :as ig]
    [xtdb.api :as xt]))
@@ -16,10 +17,22 @@
 (alias 'pass (create-ns 'juxt.pass.alpha))
 (alias 'site (create-ns 'juxt.site.alpha))
 
+(defn delete-local-access-token
+  "Until access tokens are stored in the database, restarting a server will clear
+  tokens out of memory causing the local access token to be invalid. Delete it
+  now to avoid any unhelpful 401 messages when using the CLI tool."
+  []
+  (let [f (io/file (System/getProperty "user.home") ".local/share/site/access-token.json")]
+    (when (.exists f)
+      (println "Deleting" (.getAbsolutePath f))
+      (.delete f))))
+
 (defn start []
   (println "Site by JUXT. Copyright (c) 2021, JUXT LTD.")
   (println "Compiling code, please wait...")
+  (delete-local-access-token)
   (log/info "Starting development system")
+
 
   (alter-var-root #'main/profile (constantly :dev))
   (let [system-config (main/system-config)
