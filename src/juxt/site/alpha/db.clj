5d103a09a99d4a7534056a6319a5ab514e80ce23
Malcolm Sparks <mal@juxt.pro>
2021-12-15 11:48:34 +0000

    Fix alias

--- src/juxt/site/alpha/db.clj
+++ src/juxt/site/alpha/db.clj
@@ -2,13 +2,13 @@
 
 (ns juxt.site.alpha.db
   (:require
-   [xtdb.api :as xtdb]
+   [xtdb.api :as xt]
    [integrant.core :as ig]
    [clojure.tools.logging :as log]))
 
 (defmethod ig/init-key ::xt-node [_ xtdb-opts]
   (log/info "Starting XT node")
-  (xtdb/start-node xtdb-opts))
+  (xt/start-node xtdb-opts))
 
 (defmethod ig/halt-key! ::xt-node [_ node]
   (.close node)
