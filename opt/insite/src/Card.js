426a01331def4f68d7ce94495575506f7eca9d63
Malcolm Sparks <mal@juxt.pro>
2021-12-31 11:30:32 +0000

    tweak css

--- opt/insite/src/Card.js
+++ opt/insite/src/Card.js
@@ -64,14 +64,17 @@ export default function Card() {
   const rows = [
     { title: "Method", value: data.request?.method },
     { title: "Date", value: data.request?.date },
+  ];
+
+  const graphQLRows = [
     { title: "Operation Name", value: data.request?.operationName },
   ];
 
   return (
     <div className="bg-white shadow overflow-hidden sm:rounded-lg">
       <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-6xl font-bold text-gray-900">{data.request?.status || "Unknown"}</h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">{data.request?.requestUri}</p>
+        <h3 className="text-6xl font-bold text-gray-900">{data.request?.status || "000"}</h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">{data.request?.requestUri || "<Request URI>"}</p>
       </div>
       <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
         <dl className="sm:divide-y sm:divide-gray-200">
@@ -82,9 +85,19 @@ export default function Card() {
             </div>))}
         </dl>
       </div>
-      <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg leading-6 font-medium text-gray-900">Detail</h3>
-        <div className="p-4 text-xs">
+      <div className="border-t border-gray-200 px-4 py-5 sm:px-0">
+        <h3 className="sm:px-6 text-lg leading-6 font-medium text-gray-900">GraphQL</h3>
+        <dl className="sm:divide-y sm:divide-gray-200">
+          {graphQLRows.map(({ title, value }) => (
+            <div key={title} className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">{title}</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{value}</dd>
+            </div>))}
+        </dl>
+      </div>
+      <div className="border-t border-gray-200 px-4 py-5 sm:px-0">
+        <h3 className="sm:px-6 text-lg leading-6 font-medium text-gray-900">Detail</h3>
+        <div className="sm:px-6 text-xs">
           <JSONTree data={data.request?.detail} theme={theme} />
         </div>
       </div>
