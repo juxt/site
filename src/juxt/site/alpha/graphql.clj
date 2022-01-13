juxt/site

commit 6d0509a2d86ecd7ecfdc25bf9389ff912d8b42c4

Try to generate field value on mutation prior to grabbing arg
This allows a field value to be generated based upon the initial argument, so a
generator template can be written in terms of arguments which include the field
argument in question.

src/juxt/site/alpha/graphql.clj
@@ -146,9 +146,9 @@
                (log/tracef "transform is %s" transform))
 
              (cond
-               arg-type                ; is it a singular (not a LIST)
-               (let [value (or (get args arg-name)
-                               (generate-value generator-args args))
+               arg-type                ; not a LIST
+               (let [value (or (generate-value generator-args args)
+                               (get args arg-name))
                      value
                      (cond-> value
                        ;; We don't want symbols in XT entities, because this leaks the

commit 8bfb732ef617535a27ebff7d6e6321ac9a219155

Accept given argument value before generating
The commit 6d0509a2d86ecd7ecfdc25bf9389ff912d8b42c4 broke the upsert case which
inserts or updates based on the presence of the argument value.

src/juxt/site/alpha/graphql.clj
@@ -154,9 +154,13 @@
                (log/tracef "transform is %s" transform))
 
              (cond
-               arg-type                ; not a LIST
-               (let [value (or (generate-value generator-args args)
-                               (get args arg-name))
+               arg-type                 ; not a LIST
+               (let [value (or
+                            ;; If provided we use it
+                            (get args arg-name)
+                            ;; Else we try to generate it
+                            (generate-value generator-args args)
+                            )
                      value
                      (cond-> value
                        ;; We don't want symbols in XT entities, because this leaks the