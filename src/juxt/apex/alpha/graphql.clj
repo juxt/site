79b1aaeab5d47dc1f58bcf1ecfe2d7a45ef8b79d
Malcolm Sparks <mal@juxt.pro>
2022-01-26 16:01:21 +0000

    Work in progress

--- src/juxt/apex/alpha/graphql.clj
+++ src/juxt/apex/alpha/graphql.clj
@@ -5,7 +5,8 @@
    [juxt.site.alpha.graphql :as graphql]
    [ring.util.codec :refer [form-decode]]
    [juxt.apex.alpha.representation-generation :refer [entity-body]]
-   [clojure.tools.logging :as log]))
+   [clojure.tools.logging :as log]
+   [xtdb.api :as xt]))
 
 (alias 'http (create-ns 'juxt.http.alpha))
 (alias 'apex (create-ns 'juxt.apex.alpha))
@@ -14,8 +15,9 @@
 (alias 'jinx (create-ns 'juxt.jinx.alpha))
 (alias 'grab (create-ns 'juxt.grab.alpha))
 
-(defn mutation [req]
-  (let [resource (::site/resource req)
+(defn mutation [{::site/keys [resource db uri xt-node] :as req}]
+  (let [exists-before-mutation? (xt/entity db uri)
+
         stored-query-resource-path (get-in resource [::apex/operation "x-juxt-site-graphql-query-resource"])
         stored-query-id (if (.startsWith stored-query-resource-path "/")
                           (str (:juxt.site.alpha/base-uri req) stored-query-resource-path)
@@ -27,6 +29,21 @@
         form (::apex/request-payload req)
         _ (assert form)
 
+        ;; If POST, add on ::apex/new-uri. In future we might have different
+        ;; strategies of minting a new URI for a POST, but the UUID is good for
+        ;; now and follows Site's idioms. This new-uri is useful because a POST
+        ;; often results in a single new resource being created. This is the
+        ;; right level of abstraction to determine the new URI, since it is at
+        ;; this OpenAPI layer where the URI details are known and the best place
+        ;; to decide on what this new URI is, rather than making this the
+        ;; responsibility of a lower layer such as GraphQL.
+        new-uri (when (= (:ring.request/method req) :post)
+                  (str uri (java.util.UUID/randomUUID)))
+
+        req (cond-> req
+              (= :post (:ring.request/method req))
+              (assoc ::apex/new-uri new-uri))
+
         variables (into
                    (graphql/common-variables req)
                    (concat
@@ -37,15 +54,21 @@
                     ;; TODO: Query parameters
                     form
                     ))
-        result (graphql/graphql-query
-                req
-                stored-query-id operation-name variables)]
+
+        ;; Associate GraphQL related attributes onto the request. These help
+        ;; with debugging.
+        req (into req
+                  {::site/graphql-stored-query-resource-path stored-query-resource-path
+                   ::site/graphql-operation-name operation-name
+                   ::site/graphql-variables variables})
+
+        result (graphql/graphql-query req stored-query-id operation-name variables)]
 
     (log/debugf
      "Calling GraphQL at %s operation %s with variables %s yielded %s"
      stored-query-resource-path operation-name variables (pr-str result))
 
-    (if (seq (:errors result))
+    (when (seq (:errors result))
       (throw
        (ex-info
         "Errors on GraphQL mutation"
@@ -55,27 +78,47 @@
          ::site/graphql-variables variables
          ::site/graphql-result result
          ::grab/errors (:errors result)
-         ::site/request-context
-         (into req {:ring.response/status 400
-                    ::site/graphql-stored-query-resource-path stored-query-resource-path
-                    ::site/graphql-operation-name operation-name
-                    ::site/graphql-variables variables})}))
+         ::site/request-context (assoc req :ring.response/status 400)})))
 
+    ;; The status and response is determined by both the method and which
+    ;; responses are available.
 
-      (if-let [ok-response (get-in resource [::apex/operation "responses" "200"])]
-        (assoc req
-               ::operation-name operation-name
-               :ring.response/status 200
-               :ring.response/body
-               (entity-body
+    (let [method (:ring.request/method req)
+
+          responses (get-in resource [::apex/operation "responses"])
+
+          [status response]
+          (some
+           #(find responses %)
+           (case method
+             :put [(when-not exists-before-mutation? "201") "200" "204"]
+             ;; Since POST logic should use ::site/new-uri a 201 must always be
+             ;; preferred. We could check for new-uri existence but that would
+             ;; involve awaiting the tx.
+             :post ["201" "200" "204"]
+             :delete ["200" "204"]))
+
+          _ (assert status "Status cannot be nil, badly formed response object in OpenAPI")
+
+          req (assoc req :ring.response/status (Integer/parseInt status))
+
+          resource-state (:data result)
+
+          _ (def req req)
+
+          _ (throw (ex-info "BREAK" {}))
+
+          body (entity-body
                 (assoc req
                        ;; TODO: Negotiate the required representation from the
                        ;; request. We're hardcoding html for now.
                        ::site/selected-representation
                        {::http/content-type "text/html;charset=utf-8"})
-                (:data result)))
+                resource-state
+                )
 
-        (assoc req
-               :ring.response/status 303
-               ;; TODO: Only for browsers
-               :ring.response/headers {"location" (::site/uri req)})))))
+          ;; :ring.response/headers {"location" (::site/uri req)}
+
+          ]
+
+      (assoc req :ring.response/body body))))
