056c61c1b3003494e9bc57b382fa2618679c4114
modality <modality@gmail.com>
2022-06-01 09:14:04 +0100

    import-resources idempotent

--- src/juxt/site/alpha/repl.clj
+++ src/juxt/site/alpha/repl.clj
@@ -179,17 +179,21 @@
       :else
       nil)))
 
+
 (defn import-resources
   ([] (import-resources "import/resources.edn"))
   ([filename]
-   (let [node (xt-node)
-         in (java.io.PushbackReader. (io/reader (io/input-stream (io/file filename))))]
-     (doseq [rec (resources-from-stream in)]
-       (println "Importing record" (:xt/id rec))
-       (when (:xt/id rec)
-         (xt/submit-tx node [[:xtdb.api/put rec]])))
-     (xt/sync node)
-     (println "Import finished."))))
+   (let [node (xt-node)]
+     (if (xt/entity (xt/db (xt-node)) filename)
+       (println "Skipping already imported: " filename)
+       (let [in (java.io.PushbackReader. (io/reader (io/input-stream (io/file filename))))]
+         (doseq [rec (resources-from-stream in)]
+           (println "Importing record" (:xt/id rec))
+           (when (:xt/id rec)
+             (xt/submit-tx node [[:xtdb.api/put rec]])))
+         (xt/submit-tx node [[:xtdb.api/put {:xt/id filename}]])
+         (xt/sync node)
+         (println "Import finished."))))))
 
 (defn validate-resource-line [s]
   (edn/read-string
