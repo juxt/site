64f3ef4a31d125f27ceef2cf4b593f403f016247
modality <modality@gmail.com>
2022-06-03 09:29:40 +0100

    fixed test, back to XTDB 1.20.0

--- test/juxt/site/graphql_test.clj
+++ test/juxt/site/graphql_test.clj
@@ -280,7 +280,8 @@ type Mutation {
         (is (= {:xt/id "https://example.org/persons/mal"
                 :juxt.site/type "Person"
                 :name "Malcolm Sparks"}
-               (xt/entity db "https://example.org/persons/mal"))))
+               (-> (xt/entity db "https://example.org/persons/mal")
+                   (select-keys [:xt/id :juxt.site/type :name])))))
 
       (let [body (json/read-value (:ring.response/body response))]
         (is (= {"data"
@@ -289,9 +290,7 @@ type Mutation {
                   "name" "Malcolm Sparks"}}}
                body))
         ;;body
-        ))
-
-    ))
+        ))))
 
 #_((t/join-fixtures [with-xt with-handler])
    mutation
