571537bc7bd84114e81846b518646ade4f4c0aaf
modality <modality@gmail.com>
2022-06-12 10:58:07 +0100

    missing coffee

--- src/juxt/site/alpha/db.clj
+++ src/juxt/site/alpha/db.clj
@@ -8,13 +8,14 @@
 
 (defmethod ig/init-key ::xt-node [_ xtdb-opts]
   (log/info "Starting XT node...")
-  (xt/start-node xtdb-opts)
-  ;; we need to make sure the tx-ingester has caught up before
-  ;; declaring the node up
-  (xt/await-tx xtdb-opts
-               (xt/submit-tx xtdb-opts [[::xt/put {:xt/id :tx-ingester-synced!}]]))
-  (log/info "... XT node started!")
-  )
+  (let [node (xt/start-node xtdb-opts)]
+    ;; we need to make sure the tx-ingester has caught up before
+    ;; declaring the node up
+    (->>
+     (xt/submit-tx node [[::xt/put {:xt/id :tx-ingester-synced!}]])
+     (xt/await-tx node))
+    (log/info "... XT node started!")
+    node))
 
 (defmethod ig/halt-key! ::xt-node [_ node]
   (.close node)
