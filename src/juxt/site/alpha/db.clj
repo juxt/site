6d1fa69c3483a4aa075649887936c2130d5013a1
modality <modality@gmail.com>
2022-06-12 10:34:34 +0100

    XT node sync tx-ingester

--- src/juxt/site/alpha/db.clj
+++ src/juxt/site/alpha/db.clj
@@ -7,8 +7,14 @@
    [clojure.tools.logging :as log]))
 
 (defmethod ig/init-key ::xt-node [_ xtdb-opts]
-  (log/info "Starting XT node")
-  (xt/start-node xtdb-opts))
+  (log/info "Starting XT node...")
+  (xt/start-node xtdb-opts)
+  ;; we need to make sure the tx-ingester has caught up before
+  ;; declaring the node up
+  (xt/await-tx xtdb-opts
+               (xt/submit-tx xtdb-opts [[::xt/put {:xt/id :tx-ingester-synced!}]]))
+  (log/info "... XT node started!")
+  )
 
 (defmethod ig/halt-key! ::xt-node [_ node]
   (.close node)
