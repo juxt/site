cbb7c5136d78546e128fe9802becd13eb3e6c08e
modality <modality@gmail.com>
2022-06-22 08:46:36 +0100

    deactivate skipping import

--- src/juxt/site/alpha/repl.clj
+++ src/juxt/site/alpha/repl.clj
@@ -184,7 +184,7 @@
   ([] (import-resources "import/resources.edn"))
   ([filename]
    (let [node (xt-node)]
-     (if (xt/entity (xt/db (xt-node)) filename)
+     (if 0 #_(xt/entity (xt/db (xt-node)) filename)
        (println "Skipping already imported: " filename)
        (let [in (java.io.PushbackReader. (io/reader (io/input-stream (io/file filename))))]
          (doseq [rec (resources-from-stream in)]
