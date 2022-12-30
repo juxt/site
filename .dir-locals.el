fde01d92df3ba00ccf0098eba97a9df08ca71cf9
Malcolm Sparks <mal@juxt.pro>
2022-12-30 16:04:21 +0000

    Remove deprecated CIDER configuration

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,3 @@
 ((nil
-  (cider-ns-refresh-before-fn . "dev-extras/suspend")
-  (cider-ns-refresh-after-fn  . "dev-extras/resume")
   (cider-repl-init-code . ("(start)"))
   (cider-clojure-cli-global-options . "-A:dev")))
