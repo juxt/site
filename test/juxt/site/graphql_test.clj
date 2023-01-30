9b34d674581e56ce6a271366ba0efc33b1caf719
Jamie <36566373+CirrusClouds@users.noreply.github.com>
2023-01-30 17:09:31 +0000

    Update graphql_test.clj

--- test/juxt/site/graphql_test.clj
+++ test/juxt/site/graphql_test.clj
@@ -31,21 +31,21 @@
 (defn graphql []
   (init/put-site-api! *xt-node* {::site/base-uri "https://example.org"})
   (submit-and-await!
-   [[:xtdb.api/put access-all-areas]
+    [[:xtdb.api/put access-all-areas]
 
-    [:xtdb.api/put
-     {:xt/id "https://example.org/_site/users/mal"
-      :juxt.site.alpha/type "User"
-      :juxt.pass.alpha/username "mal"
-      :email "mal@juxt.pro"
-      :name "Malcolm Sparks"}]
+     [:xtdb.api/put
+      {:xt/id "https://example.org/_site/users/mal"
+       :juxt.site.alpha/type "User"
+       :juxt.pass.alpha/username "mal"
+       :email "mal@juxt.pro"
+       :name "Malcolm Sparks"}]
 
-    [:xtdb.api/put
-     {:xt/id "https://example.org/_site/users/alx"
-      :juxt.site.alpha/type "User"
-      :juxt.pass.alpha/username "alx"
-      :email "alx@juxt.pro"
-      :name "Alex Davis"}]])
+     [:xtdb.api/put
+      {:xt/id "https://example.org/_site/users/alx"
+       :juxt.site.alpha/type "User"
+       :juxt.pass.alpha/username "alx"
+       :email "alx@juxt.pro"
+       :name "Alex Davis"}]])
 
   ;; GraphQL query (direct, for EDN)
   (let [query "{ allUsers { id } }"
@@ -54,11 +54,11 @@
         bytes (.getBytes json)
 
         r (*handler*
-           {:ring.request/method :post
-            :ring.request/path "/_site/graphql"
-            :ring.request/headers {"content-length" (str (count bytes))
-                                   "content-type" "application/json"}
-            :ring.request/body (ByteArrayInputStream. bytes)})]
+            {:ring.request/method :post
+             :ring.request/path "/_site/graphql"
+             :ring.request/headers {"content-length" (str (count bytes))
+                                    "content-type" "application/json"}
+             :ring.request/body (ByteArrayInputStream. bytes)})]
 
     (is (= 200 (:ring.response/status r)))
     (is (= {"data"
@@ -72,56 +72,58 @@
 #_((t/join-fixtures [with-xt with-handler])
    (fn []
      (submit-and-await!
-      [[:xtdb.api/put
-        {:xt/id "https://example.org/_site/users/mal",
-         :juxt.site.alpha/type "User",
-         :juxt.pass.alpha/username "mal",
-         :email "mal@juxt.pro",
-         :name "Malcolm Sparks"}]
+       [[:xtdb.api/put
+         {:xt/id "https://example.org/_site/users/mal",
+          :juxt.site.alpha/type "User",
+          :juxt.pass.alpha/username "mal",
+          :email "mal@juxt.pro",
+          :name "Malcolm Sparks"}]
 
-       [:xtdb.api/put
-        {:xt/id "https://example.org/_site/users/alx",
-         :juxt.site.alpha/type "User",
-         :juxt.pass.alpha/username "alx",
-         :email "alx@juxt.pro",
-         :name "Alex Davis"}]
+        [:xtdb.api/put
+         {:xt/id "https://example.org/_site/users/alx",
+          :juxt.site.alpha/type "User",
+          :juxt.pass.alpha/username "alx",
+          :email "alx@juxt.pro",
+          :name "Alex Davis"}]
 
-       [:xtdb.api/put
-        {:xt/id "https://example.org/_site/users/joa",
-         :juxt.site.alpha/type "User",
-         :juxt.pass.alpha/username "joa",
-         :email "joa@juxt.pro",
-         :name "Johanna Antonelli"}]
+        [:xtdb.api/put
+         {:xt/id "https://example.org/_site/users/joa",
+          :juxt.site.alpha/type "User",
+          :juxt.pass.alpha/username "joa",
+          :email "joa@juxt.pro",
+          :name "Johanna Antonelli"}]
 
-       [:xtdb.api/put
-        {:xt/id "https://example.org/_site/roles/superuser",
-         :juxt.site.alpha/type "Role",
-         :name "superuser",
-         :description "Superuser"}]
+        [:xtdb.api/put
+         {:xt/id "https://example.org/_site/roles/superuser",
+          :juxt.site.alpha/type "Role",
+          :name "superuser",
+          :description "Superuser"}]
 
-       [:xtdb.api/put
-        {:xt/id "https://example.org/_site/roles/developer",
-         :juxt.site.alpha/type "Role",
-         :name "developer",
-         :description "Developer"}]
+        [:xtdb.api/put
+         {:xt/id "https://example.org/_site/roles/developer",
+          :juxt.site.alpha/type "Role",
+          :name "developer",
+          :description "Developer"}]
 
-       [:xtdb.api/put
-        {:xt/id "https://example.org/_site/roles/superuser/users/mal",
-         ::site/type "UserRoleMapping",
-         ::pass/assignee "https://example.org/_site/users/mal",
-         ::pass/role "https://example.org/_site/roles/superuser"}]
+        [:xtdb.api/put
+         {:xt/id "https://example.org/_site/roles/superuser/users/mal",
+          ::site/type "UserRoleMapping",
+          ::pass/assignee "https://example.org/_site/users/mal",
+          ::pass/role "https://example.org/_site/roles/superuser"}]
 
-       [:xtdb.api/put
-        {:xt/id "https://example.org/_site/roles/developer/users/mal",
-         ::site/type "UserRoleMapping",
-         ::pass/assignee "https://example.org/_site/users/mal",
-         ::pass/role "https://example.org/_site/roles/developer"}]
+        [:xtdb.api/put
+         {:xt/id "https://example.org/_site/roles/developer/users/mal",
+          ::site/type "UserRoleMapping",
+          ::pass/assignee "https://example.org/_site/users/mal",
+          ::pass/role "https://example.org/_site/roles/developer"}]
 
-       [:xtdb.api/put
-        {:xt/id "https://example.org/_site/roles/superuser/users/joa",
-         ::site/type "UserRoleMapping",
-         ::pass/assignee "https://example.org/_site/users/joa",
-         ::pass/role "https://example.org/_site/roles/developer"}]])
+        [:xtdb.api/put
+         {:xt/id "https://example.org/_site/roles/superuser/users/joa",
+          ::site/type "UserRoleMapping",
+          ::pass/assignee "https://example.org/_site/users/joa",
+          ::pass/role "https://example.org/_site/roles/developer"}]
+
+        ])
 
      (let [schema-str (slurp (io/resource "juxt/site/alpha/site-schema.graphql"))
            schema (schema/compile-schema (parser/parse schema-str))
@@ -129,6 +131,7 @@
            document (document/compile-document (parser/parse query) schema)]
 
        (graphql/query schema document nil {} {::site/db (x/db *xt-node*)}))))
+
 
 (defn add-body [m s ct]
   (let [bytes (.getBytes s)]
@@ -141,54 +144,54 @@
   (let [query "query { persons { name }}"]
 
     (submit-and-await!
-     [[:xtdb.api/put access-all-areas]
+      [[:xtdb.api/put access-all-areas]
 
-      [:xtdb.api/put
-       {:xt/id "https://example.org/alice"
-        :type "Person"
-        :name "Alice"}]
+       [:xtdb.api/put
+        {:xt/id "https://example.org/alice"
+         :type "Person"
+         :name "Alice"}]
 
-      [:xtdb.api/put
-       {:xt/id "https://example.org/bob"
-        :type "Person"
-        :name "Bob"}]
+       [:xtdb.api/put
+        {:xt/id "https://example.org/bob"
+         :type "Person"
+         :name "Bob"}]
 
-      [:xtdb.api/put
-       {:xt/id "https://example.org/graphql"
-        :doc "A GraphQL endpoint"
-        :juxt.http.alpha/methods #{:post :put :options}
-        :juxt.http.alpha/acceptable "application/graphql"
-        :juxt.site.alpha/put-fn 'juxt.site.alpha.graphql/put-handler
-        :juxt.site.alpha/post-fn 'juxt.site.alpha.graphql/post-handler}]
+       [:xtdb.api/put
+        {:xt/id "https://example.org/graphql"
+         :doc "A GraphQL endpoint"
+         :juxt.http.alpha/methods #{:post :put :options}
+         :juxt.http.alpha/acceptable "application/graphql"
+         :juxt.site.alpha/put-fn 'juxt.site.alpha.graphql/put-handler
+         :juxt.site.alpha/post-fn 'juxt.site.alpha.graphql/post-handler}]
 
-      [:xtdb.api/put
-       {:xt/id "https://example.org/get-persons"
-        :doc "A GraphQL stored query"
-        :juxt.http.alpha/methods #{:put :post}
-        :juxt.http.alpha/acceptable #{"application/graphql" "application/json"}
-        :juxt.site.alpha/graphql-schema "https://example.org/graphql"
-        :juxt.site.alpha/put-fn 'juxt.site.alpha.graphql/stored-document-put-handler
-        :juxt.site.alpha/post-fn 'juxt.site.alpha.graphql/stored-document-post-handler}]
+       [:xtdb.api/put
+        {:xt/id "https://example.org/get-persons"
+         :doc "A GraphQL stored query"
+         :juxt.http.alpha/methods #{:put :post}
+         :juxt.http.alpha/acceptable #{"application/graphql" "application/json"}
+         :juxt.site.alpha/graphql-schema "https://example.org/graphql"
+         :juxt.site.alpha/put-fn 'juxt.site.alpha.graphql/stored-document-put-handler
+         :juxt.site.alpha/post-fn 'juxt.site.alpha.graphql/stored-document-post-handler}]
 
        ;; Install variants to have CSV output
-      ])
+       ])
 
     ;; Install a GraphQL schema at /graphql
     (let [schema "
 type Query { persons: [Person] @site(q: { find: [e] where: [[e {keyword: \"type\"} \"Person\"]]})}
 type Person { name: String @site(a: \"name\")}"
           response (*handler*
-                    (-> {:ring.request/method :put
-                         :ring.request/path "/graphql"}
-                        (add-body schema "application/graphql")))]
+                     (-> {:ring.request/method :put
+                          :ring.request/path "/graphql"}
+                         (add-body schema "application/graphql")))]
       (is (= 204 (:ring.response/status response))))
 
     ;; POST a query to that schema
     (let [response
           (*handler*
-           (-> {:ring.request/method :post
-                :ring.request/path "/graphql"}
-               (add-body query "application/graphql")))
+            (-> {:ring.request/method :post
+                 :ring.request/path "/graphql"}
+                (add-body query "application/graphql")))
           body (json/read-value (:ring.response/body response))]
 
       (is (= 200 (:ring.response/status response)))
@@ -196,18 +199,19 @@ type Person { name: String @site(a: \"name\")}"
 
     ;; PUT a stored query
     (*handler*
-     (-> {:ring.request/method :put
-          :ring.request/path "/get-persons"}
-         (add-body query "application/graphql")))
+      (-> {:ring.request/method :put
+           :ring.request/path "/get-persons"}
+          (add-body query "application/graphql")))
 
     ;; POST to a stored query
     #_(*handler*
-       (-> {:ring.request/method :post
-            :ring.request/path "/get-persons"}
-           (add-body (json/write-value-as-string {}) "application/json")))))
+        (-> {:ring.request/method :post
+             :ring.request/path "/get-persons"}
+            (add-body (json/write-value-as-string {}) "application/json")))))
 
 #_((t/join-fixtures [with-xt with-handler])
-   stored-query)
+   stored-query
+   )
 
 (defn stored-query-edn []
   (let [query "query { personsByKey(name: \"Bob\", key: \"name\") { name }}"]
@@ -280,6 +284,7 @@ type Person { name: String @site(a: \"name\")}"
 
 (deftest stored-query-test
   (stored-query))
+
 
 (deftest stored-query-edn-test
   (stored-query-edn))
@@ -412,7 +417,8 @@ type Mutation {
       (is (= expected-response body)))))
 
 #_((t/join-fixtures [with-xt with-handler])
-   mutation)
+   mutation
+ )
 
 (deftest mutation-test
   (mutation-base
@@ -426,8 +432,8 @@ type Mutation {
    ;; expected-response
    {"data"
     {"addPerson"
-     {:id "https://example.org/persons/mal"
-      :name "Malcolm Sparks"}}}))
+     {"id" "https://example.org/persons/mal"
+      "name" "Malcolm Sparks"}}}))
 
 (deftest mutation-with-validation-directive-test
   (mutation-base
@@ -441,8 +447,8 @@ type Mutation {
    ;; expected-response
    {"data"
     {"addPerson"
-     {:id "https://example.org/persons/mal"
-      :name "Malcolm Sparks"}}}))
+     {"id" "https://example.org/persons/mal"
+      "name" "Malcolm Sparks"}}}))
 
 (defn invalid-mutation-with-validation-directive []
   (let [schema invalid-mutation-schema
@@ -503,11 +509,12 @@ type Mutation {
 (deftest invalid-mutation-with-validation-directive-test
   (invalid-mutation-with-validation-directive))
 
+
 #_(parser/parse "mutation { addPerson { name }}")
 
 #_(schema/compile-schema
-   (parser/parse
-    "
+    (parser/parse
+      "
 schema { query: Query mutation: Mutation }
 type Query { person: Person }
 enum WorkerStatus { EMPLOYEE CONTIGENT }
@@ -522,7 +529,8 @@ type Mutation {
   addPerson(id: ID! name: String! status: WorkerStatus! = EMPLOYEE): Person
   addHoliday(person: ID beginning: Date! ending: Date! description: String): Holiday
 }
-"))
+"
+      ))
 
 (def cascade-mutation-test-data
   "Data for evict-cascade and delete-cascade tests"
@@ -550,19 +558,23 @@ type Mutation {
     {:xt/id "https://example.org/house/hh1"
      :type "House"
      :name "House 1"
-     :ownerId "https://example.org/persons/ts2"}]
+     :ownerId "https://example.org/persons/ts2"
+     }]
 
    [:xtdb.api/put
     {:xt/id "https://example.org/house/hh2"
      :type "House"
      :name "House 2"
-     :ownerId "https://example.org/persons/ts2"}]
+     :ownerId "https://example.org/persons/ts2"
+     }]
 
    [:xtdb.api/put
     {:xt/id "https://example.org/house/hh3"
      :type "House"
      :name "House 3"
-     :ownerId "https://example.org/persons/ts1"}]])
+     :ownerId "https://example.org/persons/ts1"
+     }]
+   ])
 
 (def cascade-schema
   "Schema for cascade-evict and cascade-delete tests"
@@ -593,6 +605,7 @@ type Mutation {
                  owner: Person @site(ref: \"ownerId\")
            }")
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Delete cascade test ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -615,9 +628,9 @@ mutation {
   ;; POST a query to that schema
   (comment
     (let [response (post-mutation "{ persons { id name } }")
-          body (json/read-value (:ring.response/body response))]
-      (is (= 200 (:ring.response/status response)))
-      (is (= 2 (-> body (get-in ["data" "persons"]) count)))))
+         body (json/read-value (:ring.response/body response))]
+     (is (= 200 (:ring.response/status response)))
+     (is (= 2 (-> body (get-in ["data" "persons"]) count)))))
 
   ;; Testing that db contains all data
   (is (= [(x/q (x/db *xt-node*) '{:find [e] :where [[e :type "Person"]]})
@@ -636,11 +649,11 @@ mutation {
   ;; Testing that linked entities have been deleted
   (let [linked-entities ["https://example.org/house/hh2" "https://example.org/house/hh3"]]
     (is (and (->> linked-entities
-                  (map (comp (x/q (x/db *xt-node*) '{:find [e] :where [[e :type "House"]]}) vec))
-                  (every? nil?))
+                    (map (comp (x/q (x/db *xt-node*) '{:find [e] :where [[e :type "House"]]}) vec))
+                    (every? nil?))
              (->> linked-entities
-                  (map #(x/entity-history (x/db *xt-node*) % :desc))
-                  (every? seq))))))
+                    (map #(x/entity-history (x/db *xt-node*) % :desc))
+                    (every? seq))))))
 
 (deftest delete-cascade-test
   (testing-delete-cascade))
@@ -671,7 +684,7 @@ mutation {
           #{["https://example.org/house/hh2"] ["https://example.org/house/hh1"] ["https://example.org/house/hh3"]}]))
 
   ;; Delete entity xt/id: "https://example.org/persons/ts2" and all entities with ownerId: https://example.org/pesons/ts2
-  (is (= 200 (:ring.response/status (post-mutation straight-evict-cascade-query))))
+    (is (= 200 (:ring.response/status (post-mutation straight-evict-cascade-query))))
 
   ;; Testing that entity and linked entities has been completely evicted, including all historical records
   (is (and (empty? (x/entity-history (x/db *xt-node*) "https://example.org/persons/ts2" :desc))
@@ -680,3 +693,4 @@ mutation {
 
 (deftest evict-cascade-test
   (testing-evict-cascade))
+
