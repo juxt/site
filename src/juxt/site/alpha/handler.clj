081508b3a29e865ca63aea38b46c47cc329f0cdf
Peter Wilkins <peter.wilkins@fundingcircle.com>
2023-11-23 14:00:44 +0000

    add logging to healthcheck

--- src/juxt/site/alpha/handler.clj
+++ src/juxt/site/alpha/handler.clj
@@ -1314,15 +1314,18 @@
 
 (defn xtdb-tx-lag [xt-node]
   (assert xt-node)
-  (let [tx-id (fn [tx] (get tx ::crux/tx-id 0))]
-    (- (tx-id (crux/latest-submitted-tx xt-node))
-       (tx-id (crux/latest-completed-tx xt-node)))))
+  (let [tx-id (fn [tx] (get tx ::crux/tx-id 0))
+        latest-submitted-tx (tx-id (crux/latest-submitted-tx xt-node))
+        latest-completed-tx (tx-id (crux/latest-completed-tx xt-node))]
+    (log/infof "healthcheck lag: latest-submitted-tx: %s latest-completed-tx: %s" latest-submitted-tx latest-completed-tx)
+    (- latest-submitted-tx latest-completed-tx)))
 
 (defn wrap-healthcheck
   [h]
   (fn [{::site/keys [crux-node xtdb-tx-lag-threshold check-xtdb-tx-lag-in-health-check]
         :ring.request/keys [path]
         :as req}]
+    (log/infof "remove me! flag: %s threshold: %s" check-xtdb-tx-lag-in-health-check xtdb-tx-lag-threshold)
     (if (= "/_site/healthcheck" path)
       (if (and check-xtdb-tx-lag-in-health-check
                (> (xtdb-tx-lag crux-node) xtdb-tx-lag-threshold))
